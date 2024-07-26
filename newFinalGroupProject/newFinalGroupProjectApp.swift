c93c352585f80c2f44b3751cf09d7382cd686679
Laurel <laurelpappoe@gmail.com>
2024-07-26 13:52:24 +0100

    Initial Commit

---
+++ newFinalGroupProject/newFinalGroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  newFinalGroupProjectApp.swift
+//  newFinalGroupProject
+//
+//  Created by Scholar on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct newFinalGroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
